a54cb32edf2d1d26ec590cb6cb790ee899570822
Rada <sirada_dee@cmu.ac.th>
2025-12-23 20:38:37 +0700

    lab10_2.cpp completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -10,6 +10,13 @@ int main (){
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	string line ;
+	dest << "-------------------- BOOM ---------------------\n";
+	while (getline(source,line))
+	{
+		dest << line << endl ;
+	}
+	dest << "-------------------- HA!! ---------------------" ;
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
     source.close();
